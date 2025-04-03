d0deed2cbccc2cefdae7dded453cb73320152853
Helvio Junior <helvio_junior@hotmail.com>
2025-04-03 20:55:26 -0300

    BugFix

---
+++ pkg/gopcap2/utils.go
@@ -0,0 +1,56 @@
+/*
+ * GOPACP - PCAP file parsing in Golang
+ * Copyright (c) 2017 Philipp Mieden <dreadl0ck [at] protonmail [dot] ch>
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *
+ * ## Disclaimer
+ * This submodule has copied from his original repo https://github.com/dreadl0ck/gopcap
+ */
+
+package gopcap
+
+import "io"
+
+/////////////////////////////
+// Utils
+/////////////////////////////
+
+// Count packets in file
+// TODO: dont actually read anything... just seek to next offset
+func Count(path string) (int64, error) {
+
+	var (
+		numPackets int64
+		r, err     = Open(path)
+	)
+	if err != nil {
+		return numPackets, err
+	}
+
+	for {
+		_, _, err := r.ReadNextPacket()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return numPackets, err
+			}
+		}
+		numPackets++
+	}
+
+	err = r.Close()
+	if err != nil {
+		return numPackets, err
+	}
+
+	return numPackets, nil
+}
